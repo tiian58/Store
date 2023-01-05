1d579aab86731f6bcc46c53f8bfbdc9b67bdbfac
Tiian58 <tiian58@gmail.com>
2023-01-05 12:41:36 -0300

    post endpoint

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from models.product import Product
 
 app = Flask(__name__)
 
-products = {
-     1:{"id":1, "Name": "Coca-Cola", "Amount": 5},
-     2:{"id":2, "Name": "Cookie", "Amount": 1}
-}
+#products = {
+#     1:{"id":1, "Name": "Coca-Cola", "Amount": 5},
+#     2:{"id":2, "Name": "Cookie", "Amount": 1}
+#}
+products = [
+     {"Id":1, "Name": "Coca-Cola", "Amount": 5},
+     {"Id":2, "Name": "Cookie", "Amount": 1}
+]
 
 @app.route("/", methods=["GET"])
 def welcome():
@@ -17,11 +21,32 @@ def welcome():
 def get_products():
     data = products
     if data:
+        return jsonify(data), 200
+    elif data == False:
+        return jsonify({'message': 'Error'}), 500
+    else:
+        return jsonify({'products': {}})
+
+@app.route("/products", methods=["POST"])
+def insert_products():
+    products.append(request.get_json())
+    if products:
+        return jsonify(products), 201
+    elif products == False:
+        return jsonify({'message': 'Error'}), 500
+    else:
+        return jsonify({'products': {}})
+
+@app.route("/products/<id>", methods=["GET"])
+def get_products_id():
+    data = id
+    if data:
         return jsonify(data)
     elif data == False:
         return jsonify({'message': 'Error'})
     else:
         return jsonify({'products': {}})
 
+
 if __name__== "__main__":
-    app.run(port = 5000, debug=True)
+    app.run(port = 5001, debug=True)
